d3ba4a8093e6adad6ad28ee229c5bc58f6ab0d76
Leong Wei Men <>
2024-03-04 01:10:21 +0800

    delete unuse comment

--- app/src/main/java/com/example/omdb/ui/theme/movielist/MovieListFragment.kt
+++ app/src/main/java/com/example/omdb/ui/theme/movielist/MovieListFragment.kt
@@ -34,14 +34,6 @@ class MovieListFragment : Fragment(), ListClickListener {
     private lateinit var myAdapter: RecyclerView.Adapter<*>
 
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//
-
-//
-//    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -55,45 +47,11 @@ class MovieListFragment : Fragment(), ListClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        movieList = ArrayList()
 
-//        recyclerView =binding.recyclerView
-//        recyclerViewMovieAdapter = RecyclerViewMovieAdapter(this, movieList)
-//        val layoutManager: RecyclerView.LayoutManager = GridLayoutManager(context, 2)
-//        recyclerView!!.layoutManager = layoutManager
-//        recyclerView!!.adapter = recyclerViewMovieAdapter
-
-//        prepareMovieListData()
         getAllData()
     }
 
-//    private fun prepareMovieListData() {
-//        var movie = Movie("Avatar", R.drawable.avatar)
-//        movieList.add(movie)
-//        movie = Movie("Batman", R.drawable.avatar)
-//        movieList.add(movie)
-//
-//        movie = Movie("End Game", R.drawable.avatar)
-//        movieList.add(movie)
-//        movie = Movie("Hulk", R.drawable.avatar)
-//        movieList.add(movie)
-//        movie = Movie("Inception", R.drawable.avatar)
-//        movieList.add(movie)
-//        movie = Movie("Jumanji", R.drawable.avatar)
-//        movieList.add(movie)
-//        movie = Movie("Lucy", R.drawable.avatar)
-//        movieList.add(movie)
-//        movie = Movie("Jurassic World", R.drawable.avatar)
-//        movieList.add(movie)
-//        movie = Movie("Spider Man", R.drawable.avatar)
-//        movieList.add(movie)
-//        movie = Movie("Venom", R.drawable.avatar)
-//        movieList.add(movie)
-//
-//        recyclerViewMovieAdapter!!.notifyDataSetChanged()
-//
-//        //All Code is done let's run the app
-//    }
+
     fun getAllData(){
 
         Api.retrofitService.getAllData().enqueue(object: Callback<MoviesPageModel> {
